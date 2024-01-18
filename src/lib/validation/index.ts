4a84efbbd275d3906b34689cb072718fee215bd4
whyDoILoveThis <136662855+whyDoILoveThis@users.noreply.github.com>
2024-01-18 16:57:12 -0600

    create sign up form

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,8 @@
+import * as z from "zod";
+
+export const SignupValidation = z.object({
+    name: z.string().min(2, {message: "Too Short!🤏"}).max(100),
+    username: z.string().min(2,{message: "Too Short!🤏"}).max(100),
+    email: z.string().email(),
+    password: z.string().min(8, {message: "Password must be atleaste 8 characters"})
+  });
